780d9d670c52bf99eff182cbc8851ddd3d883952
Adam Juraszek <43826078+Rychoc@users.noreply.github.com>
2022-02-02 02:18:46 +0100

    WinForms Game Aplication

---
+++ Egzaminator/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Egzaminator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanelGrid = new System.Windows.Forms.FlowLayoutPanel();
+            this.lableProgress = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lableGameRules = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.flowLayoutPanelRules = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanelRules.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelGrid
+            // 
+            this.flowLayoutPanelGrid.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanelGrid.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.flowLayoutPanelGrid.Location = new System.Drawing.Point(108, 158);
+            this.flowLayoutPanelGrid.Name = "flowLayoutPanelGrid";
+            this.flowLayoutPanelGrid.Size = new System.Drawing.Size(470, 460);
+            this.flowLayoutPanelGrid.TabIndex = 0;
+            // 
+            // lableProgress
+            // 
+            this.lableProgress.BackColor = System.Drawing.Color.Transparent;
+            this.lableProgress.Font = new System.Drawing.Font("Candara", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lableProgress.Location = new System.Drawing.Point(75, 430);
+            this.lableProgress.Margin = new System.Windows.Forms.Padding(75, 0, 0, 0);
+            this.lableProgress.Name = "lableProgress";
+            this.lableProgress.Size = new System.Drawing.Size(290, 20);
+            this.lableProgress.TabIndex = 3;
+            this.lableProgress.Text = "Zapełnienie sali obserwatorami:";
+            this.lableProgress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.progressBar.Location = new System.Drawing.Point(60, 453);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(60, 3, 3, 3);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(335, 15);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 0;
+            // 
+            // lableGameRules
+            // 
+            this.lableGameRules.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lableGameRules.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lableGameRules.Location = new System.Drawing.Point(75, 100);
+            this.lableGameRules.Margin = new System.Windows.Forms.Padding(75, 100, 0, 0);
+            this.lableGameRules.Name = "lableGameRules";
+            this.lableGameRules.Size = new System.Drawing.Size(290, 330);
+            this.lableGameRules.TabIndex = 7;
+            this.lableGameRules.Text = resources.GetString("lableGameRules.Text");
+            this.lableGameRules.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.Transparent;
+            this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonStart.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.ForeColor = System.Drawing.Color.Black;
+            this.buttonStart.Location = new System.Drawing.Point(60, 476);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(60, 5, 0, 0);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(335, 60);
+            this.buttonStart.TabIndex = 11;
+            this.buttonStart.Text = "Zacznij grę!";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // flowLayoutPanelRules
+            // 
+            this.flowLayoutPanelRules.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanelRules.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanelRules.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("flowLayoutPanelRules.BackgroundImage")));
+            this.flowLayoutPanelRules.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.flowLayoutPanelRules.Controls.Add(this.lableGameRules);
+            this.flowLayoutPanelRules.Controls.Add(this.lableProgress);
+            this.flowLayoutPanelRules.Controls.Add(this.progressBar);
+            this.flowLayoutPanelRules.Controls.Add(this.buttonStart);
+            this.flowLayoutPanelRules.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.flowLayoutPanelRules.Location = new System.Drawing.Point(672, 1);
+            this.flowLayoutPanelRules.Name = "flowLayoutPanelRules";
+            this.flowLayoutPanelRules.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.flowLayoutPanelRules.Size = new System.Drawing.Size(435, 630);
+            this.flowLayoutPanelRules.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1108, 630);
+            this.Controls.Add(this.flowLayoutPanelRules);
+            this.Controls.Add(this.flowLayoutPanelGrid);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sesja Egzaminator";
+            this.flowLayoutPanelRules.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelGrid;
+        private System.Windows.Forms.Label lableProgress;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lableGameRules;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelRules;
+    }
+}
+
---
+++ Egzaminator/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Egzaminator
+{
+    public partial class Form1 : Form
+    {
+        Grid[,] arrayGrid = new Grid[7, 7];
+        string read;
+        Element thing;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = false;
+            Random rnd = new Random();
+            int version = rnd.Next(5);
+            int value = 0;
+            int chairs = 0;
+            for (int i = 0; i <= 48; i++, Thread.Sleep(50))
+            {
+                Button buttonGrid = new Button();
+                buttonGrid.Name = "buttonGrid" + i.ToString();
+                buttonGrid.Size = new System.Drawing.Size(60, 60);
+                buttonGrid.BackgroundImageLayout = ImageLayout.Stretch;
+                buttonGrid.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+                buttonGrid.ForeColor = System.Drawing.Color.White;
+                buttonGrid.UseVisualStyleBackColor = true;
+                if (Properties.Resources.gridValues1[value] == ' ')
+                    value++;
+                switch (version)
+                {
+                    case 0:
+                        read = Properties.Resources.gridValues1[value++].ToString();
+                        break;
+                    case 1:
+                        read = Properties.Resources.gridValues2[value++].ToString();
+                        break;
+                    case 2:
+                        read = Properties.Resources.gridValues3[value++].ToString();
+                        break;
+                    case 3:
+                        read = Properties.Resources.gridValues4[value++].ToString();
+                        break;
+                    default:
+                        read = Properties.Resources.gridValues5[value++].ToString();
+                        break;
+                }
+                if (read == "-")
+                {
+                    buttonGrid.Tag = true;
+                    buttonGrid.Text = " ";
+                    buttonGrid.BackgroundImage = Properties.Resources.chairs;
+                    buttonGrid.Enabled = true;
+                    buttonGrid.BackColor = System.Drawing.Color.Transparent;
+                    chairs++;
+                    thing = Element.chair;
+                }
+                else
+                {
+                    buttonGrid.Text = read;
+                    buttonGrid.BackgroundImage = null;
+                    buttonGrid.Enabled = false;
+                    buttonGrid.BackColor = System.Drawing.Color.DimGray;
+                    buttonGrid.Tag = false;
+                    if (read == "x")
+                        thing = Element.Phillarx;
+                    else if (read == "0")
+                        thing = Element.Phillar0;
+                    else if (read == "1")
+                        thing = Element.Phillar1;
+                    else if (read == "2")
+                        thing = Element.Phillar2;
+                    else if (read == "3")
+                        thing = Element.Phillar3;
+                    else
+                        thing = Element.chair;
+                }
+                buttonGrid.Click += buttonGrid_Click;
+                flowLayoutPanelGrid.Controls.Add(buttonGrid);
+                Grid tempGrid = new Grid(buttonGrid, thing);
+                arrayGrid[i / 7, i % 7] = tempGrid;
+                flowLayoutPanelGrid.Update();
+            }
+            progressBar.Maximum = chairs;
+        }
+
+        private void buttonGrid_Click(object sender, EventArgs e)
+        {
+            Button buttonGrid = (Button)sender;
+            bool tag = (bool)buttonGrid.Tag;
+            int gridNumber = int.Parse(buttonGrid.Name.Substring(10));
+            int solution = 0;
+            bool can = true;
+
+            if (buttonGrid.BackColor == System.Drawing.Color.Khaki)
+                can = false;
+
+            if (gridNumber / 7 + 1 < 7 && arrayGrid[gridNumber / 7 + 1, gridNumber % 7].element != Element.chair && can)
+            {
+                //check DOWN
+                if (arrayGrid[gridNumber / 7 + 1, gridNumber % 7].element != Element.chair || arrayGrid[gridNumber / 7 + 1, gridNumber % 7].element != Element.Phillarx)
+                {
+                    Check tempCheck = new Check();
+                    solution = Check.Checking(arrayGrid, gridNumber / 7 + 1, gridNumber % 7);
+                }
+                _ = solution < 0 ? can = false : can = true;
+            }
+
+            if (gridNumber / 7 - 1 >= 0 && arrayGrid[gridNumber / 7 - 1, gridNumber % 7].element != Element.chair && can)
+            {
+                //check UP
+                if (arrayGrid[gridNumber / 7 - 1, gridNumber % 7].element != Element.chair || arrayGrid[gridNumber / 7 + 1, gridNumber % 7].element != Element.Phillarx)
+                {
+                    Check tempCheck = new Check();
+                    solution = Check.Checking(arrayGrid, gridNumber / 7 - 1, gridNumber % 7);
+                }
+                _ = solution < 0 ? can = false : can = true;
+            }
+
+            if (gridNumber % 7 + 1 < 7 && arrayGrid[gridNumber / 7, gridNumber % 7 + 1].element != Element.chair && can)
+            {
+                //check RIGHT
+                if (arrayGrid[gridNumber / 7, gridNumber % 7 + 1].element != Element.chair || arrayGrid[gridNumber / 7 + 1, gridNumber % 7].element != Element.Phillarx)
+                {
+                    Check tempCheck = new Check();
+                    solution = Check.Checking(arrayGrid, gridNumber / 7, gridNumber % 7 + 1);
+                }
+                _ = solution < 0 ? can = false : can = true;
+            }
+
+            if (gridNumber % 7 - 1 >= 0 && arrayGrid[gridNumber / 7, gridNumber % 7 - 1].element != Element.chair && can)
+            {
+                //check LEFT
+                if (arrayGrid[gridNumber / 7, gridNumber % 7 - 1].element != Element.chair || arrayGrid[gridNumber / 7 + 1, gridNumber % 7].element != Element.Phillarx)
+                {
+                    Check tempCheck = new Check();
+                    solution = Check.Checking(arrayGrid, gridNumber / 7, gridNumber % 7 - 1);
+                }
+                _ = solution < 0 ? can = false : can = true;
+            }
+
+            if (!can)
+            {
+                MessageBox.Show("Nie można tutaj ustawić obserwatora!");
+                buttonGrid.Enabled = false;
+            }
+            else
+            {
+                if (tag)
+                {
+                    for (int i = 1; i < 7; i++)
+                    {
+                        if (gridNumber / 7 + i < 7 && arrayGrid[gridNumber / 7 + i, gridNumber % 7].element == Element.chair)
+                        {
+                            if (arrayGrid[gridNumber / 7 + i, gridNumber % 7].button.BackColor != System.Drawing.Color.Khaki)
+                                progressBar.PerformStep();
+                            arrayGrid[gridNumber / 7 + i, gridNumber % 7].button.BackColor = System.Drawing.Color.Khaki;
+                        }
+                        else
+                            break;
+                    }
+                    for (int i = 1; i < 7; i++)
+                    {
+                        if (gridNumber / 7 - i >= 0 && arrayGrid[gridNumber / 7 - i, gridNumber % 7].element == Element.chair)
+                        {
+                            if (arrayGrid[gridNumber / 7 - i, gridNumber % 7].button.BackColor != System.Drawing.Color.Khaki)
+                                progressBar.PerformStep();
+                            arrayGrid[gridNumber / 7 - i, gridNumber % 7].button.BackColor = System.Drawing.Color.Khaki;
+                        }
+                        else
+                            break;
+                    }
+                    for (int i = 1; i < 7; i++)
+                    {
+                        if (gridNumber % 7 + i < 7 && arrayGrid[gridNumber / 7, gridNumber % 7 + i].element == Element.chair)
+                        {
+                            if (arrayGrid[gridNumber / 7, gridNumber % 7 + i].button.BackColor != System.Drawing.Color.Khaki)
+                                progressBar.PerformStep();
+                            arrayGrid[gridNumber / 7, gridNumber % 7 + i].button.BackColor = System.Drawing.Color.Khaki;
+                        }
+                        else
+                            break;
+                    }
+                    for (int i = 1; i < 7; i++)
+                    {
+                        if (gridNumber % 7 - i >= 0 && arrayGrid[gridNumber / 7, gridNumber % 7 - i].element == Element.chair)
+                        {
+                            if (arrayGrid[gridNumber / 7, gridNumber % 7 - i].button.BackColor != System.Drawing.Color.Khaki)
+                                progressBar.PerformStep();
+                            arrayGrid[gridNumber / 7, gridNumber % 7 - i].button.BackColor = System.Drawing.Color.Khaki;                         
+                        }
+                        else
+                            break;
+                    }
+                    buttonGrid.BackgroundImage = Properties.Resources.chair;
+                    buttonGrid.BackColor = System.Drawing.Color.Yellow;
+                    thing = Element.observer;
+                    Grid tempGrid = new Grid(buttonGrid, thing);
+                    arrayGrid[gridNumber / 7, gridNumber % 7] = tempGrid;
+                    buttonGrid.Enabled = false;
+                    progressBar.PerformStep();
+                }
+            }
+            int canWePlay = 0;
+            for (int i = 0; i < 49; i++)
+            {
+                if (arrayGrid[i / 7, i % 7].button.Enabled == true && arrayGrid[i / 7, i % 7].button.BackColor != System.Drawing.Color.Khaki)
+                    canWePlay++;
+                if (canWePlay > 0)
+                    break;
+            }
+            if (progressBar.Value == progressBar.Maximum || canWePlay == 0)
+            {
+                if (progressBar.Value == progressBar.Maximum)
+                    canWePlay--;
+                string messageTitle = canWePlay == 0 ? "Co za pech..." : "Gratulacje!";
+                var result = MessageBox.Show("Czy chcesz zagrać jeszcze raz?",
+                    messageTitle,
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+                if (result == DialogResult.No)
+                    this.Close();
+                else
+                    flowLayoutPanelGrid.Controls.Clear();
+                progressBar.Value = 0;
+                buttonStart.Enabled = true;
+            }
+        }
+    }
+    public enum Element
+    {
+        chair,
+        observer,
+        Phillarx,
+        Phillar0,
+        Phillar1,
+        Phillar2,
+        Phillar3
+    }
+
+    public class Grid
+    {
+        public Button button { get; set; }
+        public Element element;
+        public Grid(Button button, Element element)
+        {
+            this.button = button;
+            this.element = element;
+        }
+    }
+    class Check
+    {
+        public static int Checking(Grid[,] arrayGrid, int checkwidth, int checkhight)
+        {
+            int count = 0;
+            if (arrayGrid[checkwidth, checkhight].element == Element.Phillar3)
+                count = 2;
+            if (arrayGrid[checkwidth, checkhight].element == Element.Phillar2)
+                count = 1;
+            if (arrayGrid[checkwidth, checkhight].element == Element.Phillar1)
+                count = 0;
+            if (arrayGrid[checkwidth, checkhight].element == Element.Phillar0)
+                count = -1;
+            if (arrayGrid[checkwidth, checkhight].element != Element.Phillarx)
+            {
+                //chceck near
+                if (checkwidth + 1 < 7 && arrayGrid[checkwidth + 1, checkhight].element == Element.observer)
+                    count--;
+                if (checkwidth - 1 >= 0 && arrayGrid[checkwidth - 1, checkhight].element == Element.observer)
+                    count--;
+                if (checkhight + 1 < 7 && arrayGrid[checkwidth, checkhight + 1].element == Element.observer)
+                    count--;
+                if (checkhight - 1 >= 0 && arrayGrid[checkwidth, checkhight - 1].element == Element.observer)
+                    count--;
+            }
+            return count;
+        }
+    }
+}
